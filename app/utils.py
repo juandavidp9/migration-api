e009788d732fce965ce4c848872de98eb96b41e2
juandavidp9 <juandavidp94@gmail.com>
2025-02-01 16:46:52 -0500

    create util function parse_csv

---
+++ app/utils.py
@@ -0,0 +1,47 @@
+from io import StringIO
+import csv
+from datetime import datetime
+from pydantic import ValidationError
+from sqlalchemy.exc import IntegrityError
+from fastapi import HTTPException
+import os
+
+def parse_csv(file, model, db):
+    """
+    Parsea un archivo CSV y valida cada fila según el modelo Pydantic.
+    Retorna una lista de objetos válidos y una lista de errores.
+    """
+    csv_data = []
+    errors = []
+    reader = csv.reader(StringIO(file.decode('utf-8')))
+    
+    for row_number, row in enumerate(reader, start=1):
+        if len(row) != len(model.__fields__):
+            errors.append(f"Fila {row_number}: Número incorrecto de columnas. Se esperaban {len(model.__fields__)}, se encontraron {len(row)}")
+            continue
+        
+        try:
+            # Convertir y validar la fila usando el modelo Pydantic
+            if model.__name__ == "EmployeeCreate":
+
+                # Validar que department_id y job_id existan en la base de datos
+                department_id = int(row[3]) if row[3] else None
+                job_id = int(row[4]) if row[4] else None
+
+                if department_id and not db.query(models.Department).filter(models.Department.id == department_id).first():
+                    errors.append(f"Fila {row_number}: department_id {department_id} no existe en la base de datos.")
+                    continue
+
+                if job_id and not db.query(models.Job).filter(models.Job.id == job_id).first():
+                    errors.append(f"Fila {row_number}: job_id {job_id} no existe en la base de datos.")
+                    continue
+
+                row[2] = datetime.fromisoformat(row[2]) if row[2] else None
+
+            csv_data.append(model(**dict(zip(model.__fields__.keys(), row))))
+        except ValidationError as e:
+            errors.append(f"Fila {row_number}: Error de validación - {str(e)}")
+        except ValueError as e:
+            errors.append(f"Fila {row_number}: Error de valor - {str(e)}")
+    
+    return csv_data, errors
